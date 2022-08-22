448269a6f0f3b1bdd9a6b768395857bd593fe06b
rafaelsaavedraat <46793459+rafaelsaavedraat@users.noreply.github.com>
2022-08-22 15:17:13 -0500

    inactive /produ

--- src/routes/products.routers.js
+++ src/routes/products.routers.js
@@ -1,7 +1,7 @@
 import {Router} from "express";
 import {createnewProducts, getProducts, listaprec} from "../controllers/products.controller"
 const router = Router();
-router.get('/products', getProducts);
+//router.get('/products', getProducts);
 
 router.post('/listaprec', listaprec);
 //router.post('/products', createnewProducts)
